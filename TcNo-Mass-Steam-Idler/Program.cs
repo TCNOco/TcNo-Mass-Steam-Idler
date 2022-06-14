aca084dd7d14890e99ae6f66352faf8a3636e087
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2022-06-14 17:07:38 +0200

    Init

---
+++ TcNo-Mass-Steam-Idler/Program.cs
@@ -0,0 +1,66 @@
+﻿using Steamworks;
+using System.Diagnostics;
+
+// Set Working Directory to same as self
+Directory.SetCurrentDirectory(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) ?? "");
+
+
+var appIds = new List<string>();
+
+// Read appIds.txt lines into list, if it exists
+if (File.Exists("appids.txt"))
+{
+    appIds = string.Join(",", File.ReadAllLines("appids.txt").ToList()).Replace("\r\n", ",").Replace("\n", ",").Replace(",,", ",").Split(',').ToList();
+}
+
+if (appIds.Count == 0)
+{
+    Console.Write("Please enter a list of AppIDs (Comma seperated): ");
+    var response = Console.ReadLine();
+    if (response is null || response.Trim().Length < 1)
+    {
+        Console.WriteLine("No AppIDs entered. Press any key to exit...");
+        Console.ReadKey();
+        Environment.Exit(0);
+    }
+    appIds = response.Split(',').ToList();
+}
+
+// Check if Steam is running
+if (!SteamAPI.IsSteamRunning())
+{
+    Console.WriteLine("Steam not running. Press any key to exit...");
+    Console.ReadKey();
+    Environment.Exit(0);
+}
+
+int waitTime = 0;
+while (waitTime is 0)
+{
+    Console.Write("Idle each game for (1-99) seconds: ");
+    int.TryParse(Console.ReadLine(), out waitTime);
+    waitTime = waitTime * 1000 + 200;
+}
+
+
+Console.WriteLine("Starting idling!");
+
+foreach (var appId in appIds)
+{
+    // Cleanup - Remove spaces from each appId
+    var a = appId.Trim();
+    File.WriteAllText("steam_appid.txt", a);
+
+    // Start game.exe
+    string sysFolder = Environment.GetFolderPath(Environment.SpecialFolder.System);
+    ProcessStartInfo pInfo = new ProcessStartInfo();
+    pInfo.FileName = "idle.exe";
+    pInfo.Arguments = waitTime + " " + appId;
+
+    Console.WriteLine($"Idling: {a}, for 5 seconds...");
+
+    Process p = Process.Start(pInfo);
+    p.WaitForExit();
+}
+
+Console.ReadKey();
---
+++ idle/Program.cs
@@ -0,0 +1,31 @@
+﻿using Steamworks;
+
+int waitTime = 0;
+int appId = 0;
+
+// Get arguments
+string[] arguments = Environment.GetCommandLineArgs();
+foreach (var a in arguments)
+{
+    int value = 0;
+    int.TryParse(a, out value);
+    if (value == 0)
+        continue;
+
+    if (waitTime == 0)
+    {
+        waitTime = value;
+        continue;
+    }
+
+    if (appId == 0)
+    {
+        appId = value;
+        continue;
+    }
+}
+
+Console.WriteLine("Idling game: " + appId);
+
+SteamAPI.Init();
+Thread.Sleep(waitTime);
